115bd0b628a0409e0f37c712a7e6a9c83e0f9636
Aashiq <ashik555sk@gmail.com>
2025-10-02 14:23:40 +0530

    hello program

---
+++ C++/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the input/output stream
+
+using namespace std; // Use the standard namespace
+
+int main()
+{
+    // Output the message to the console
+    cout << "Hello, World!" << endl;
+    return 0; // Return 0 to indicate successful execution
+}
