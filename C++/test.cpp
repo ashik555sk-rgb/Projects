ed0517df0bbfd01430d6f58c734c96fc29598063
Aashiq <ashik555sk@gmail.com>
2025-10-04 21:41:21 +0530

    added a new c++ program

---
+++ C++/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the input/output stream
+
+using namespace std; // Use the standard namespace
+
+int main()
+{
+    // Output the message to the console
+    cout << "This is my first C++ Program." << endl;
+    return 0; // Return 0 to indicate successful execution
+}
