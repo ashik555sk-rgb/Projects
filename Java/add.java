ba7ca6fe3da9688a8552a3d83f85789ae104dd40
Aashiq <ashik555sk@gmail.com>
2025-10-06 19:00:12 +0530

    add the file add1 of java

--- Java/add.java
+++
@@ -1,9 +0,0 @@
-public class add{
-public Static void main (String[] rgs){
-    int a = 5;
-    int b = 7;
-    int c = 8;
-    int add = a+b+c;
-    System.out.println("THE ADDITION IS " + add);
-}
-}
