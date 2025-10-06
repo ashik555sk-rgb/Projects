975a2239d287046676969210de3d4868b7dff1d3
Aashiq <ashik555sk@gmail.com>
2025-10-06 20:26:12 +0530

    add practice in java

--- Java/add1.java
+++ Java/add1.java
@@ -1,10 +1,10 @@
 public class add1 {
-    public static void main (String[] args){
-        int a = 5;
+    public static void main(String[] args) {
+        byte a = 5;
         int b = 9;
         int c = 4;
         int d = a + b + c;
         System.out.println("Addition is: " + d);
     }
-    
+
 }
