6f3c73b2ab1a9b1f5d0f9f11fe398d4460f30b05
Aashiq <ashik555sk@gmail.com>
2025-10-06 20:25:50 +0530

    add practice file

---
+++ Java/practice.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class practice {
+    /**
+     * @param args
+     */
+    public static void main (String[] args){
+
+    //    // Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter your  first subject marks");
+    //     int a = sc.nextInt();
+    //     System.out.println("Enter your second subject marks");
+    //     int b = sc.nextInt();
+    //     System.out.println("Enter your THoird Subject marks");
+    //     int c = sc.nextInt();
+    //     System.out.println("Enter your Fourth subject marks");
+    //     int d = sc.nextInt();           
+    //     System.out.println("Enter your Fifth subject marks");
+    //     int e = sc.nextInt();
+    //     int total = a+b+c+d+e;
+    //     double per = total/5;
+    //     System.out.println("Your percentage is " + per);
+    // float a = 89;
+    // float b = 77;
+    // float c = 78;
+    // float sum = a+b+c;
+    // double  cgpa = sum/30;
+    // System.out.println("Your CGPA is " + cgpa);
+    // question 3
+    // Scanner input = new Scanner(System.in);
+    // System.out.println("Enter your name");
+    // String name = input.next();
+    // System.out.println("hello "  +  name  +  " have a good day");
+    //question 5
+    // Scanner input = new Scanner(System.in);
+    // System.out.println("Enter your number");
+    // boolean b = input.hasNextInt();
+    // System.out.println(b);
+    // question 4
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter your Kilometers");
+    double km= input.nextDouble();
+    double miles = km*0.621371;
+    System.out.println("YOUR " + km +  " KILOMETER IS " + miles + " MILES " );
+
+    }
+}
