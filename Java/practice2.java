03b274c60b95cf6b2f9f3380295e9af5b96b196c
Aashiq <ashik555sk@gmail.com>
2025-10-07 20:39:38 +0530

    add java file practice2

---
+++ Java/practice2.java
@@ -0,0 +1,13 @@
+//precidence and associativity
+public class practice2 {
+    public static void main(String[] args) {
+        // int a = 9*8-8/4;
+        // System.out.println(a);
+        int a = 6;
+        int b = 5;
+        int z= a*b/a;
+        int c = a/b*a;
+        System.out.println(z);
+        System.out.println(c);
+    }
+}
