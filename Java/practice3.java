26c4ff83f6b035d3f1fcf320d84192a44824a504
Aashiq <ashik555sk@gmail.com>
2025-10-08 11:23:22 +0530

    add java program practice3

---
+++ Java/practice3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class practice3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your Name : ");
+        // String s = sc.next();
+        // System.out.println(s);
+        System.out.println("Enter Your Sentence");
+        String c =sc.nextLine();
+        System.out.println(c);
+    }
+    
+}
