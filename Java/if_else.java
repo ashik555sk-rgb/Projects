cb0a75d35af4dc7f1d72702c798abbed6713180e
Aashiq <ashik555sk@gmail.com>
2025-10-10 20:39:20 +0530

    add java file if_else into the git

---
+++ Java/if_else.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class if_else {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("ENTER YOUR AGE: ");
+        int age = input.nextInt();
+        boolean result = age>=18&&age<=60;
+        if(result){
+            System.out.println("YOU ARE ELEGIBLE FOR Driving);");
+
+        }
+        else{
+            System.out.println("YOUR ARE NOT ELEGIBLE FOR Driving");
+        }
+
+    }
+}
