9aa18bd5d1fefffd49674d27fd6232ffd013395b
Aashiq <ashik555sk@gmail.com>
2025-10-11 20:41:25 +0530

    add the java file logical_operator

---
+++ Java/logical_operator.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class logical_operator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    //AND OPERATOR;
+//     boolean a= true;
+//     boolean b = true;
+//     if( a&&b){
+// System.out.println("A aND B is True");
+//     }
+//     else{
+//         System.out.println("A aND B is False");
+
+//     }
+
+    //OR OPERATOR; in OR operator minimum one condition is true then it will return true;
+    boolean a = true;
+    boolean b = true;
+    if(a||b)
+    {
+        System.out.println( "true");
+    }
+    else{
+         System.out.println("fAlse");
+    }
+}
+}
