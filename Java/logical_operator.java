f20a9eff126e27ee879401db7914938fa91ed653
Aashiq <ashik555sk@gmail.com>
2025-10-12 18:51:17 +0530

    add the file java program switchExample

--- Java/logical_operator.java
+++ Java/logical_operator.java
@@ -14,14 +14,20 @@ public class logical_operator {
 //     }
 
     //OR OPERATOR; in OR operator minimum one condition is true then it will return true;
+    // boolean a = true;
+    // boolean b = true;
+    // if(a||b)
+    // {
+    //     System.out.println( "true");
+    // }
+    // else{
+    //      System.out.println("fAlse");
+    // }
+                     // not operator
     boolean a = true;
     boolean b = true;
-    if(a||b)
-    {
-        System.out.println( "true");
-    }
-    else{
-         System.out.println("fAlse");
-    }
+    System.out.println(!a);
+    System.out.println(!b);
+    
 }
 }
