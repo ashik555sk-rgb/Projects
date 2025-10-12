c164f5168acbb83e3d0e77eb3d0b6b867d9080cc
Aashiq <ashik555sk@gmail.com>
2025-10-12 20:53:30 +0530

    add the java program questions.java

---
+++ Java/questions.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class questions {
+    public static void main(String[] args) {
+        // QUESTION 1;
+        // int a = 10;
+        // if(a==11){
+        // System.out.println("I am 11");
+        // }
+        // else{
+        // System.out.println("I am not 11");
+        // }
+        // QUESTION 2;
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your first Subject marks");
+        // int sub1 = sc.nextInt();
+        // System.out.println("Enter your second Subject marks");
+        // int sub2 = sc.nextInt();
+        // System.out.println("Enter your third Subject marks");
+        // int sub3 = sc.nextInt();
+        // float avg = (sub1 + sub2 + sub3) / 3.0f;
+        // if (avg >= 40 && sub1 >= 33 && sub2 >= 33 && sub3 >= 33) {
+        //     System.out.println("YOU ARE PASS WITH: " + avg + "%");
+
+        // } else {
+        //     System.out.println("YOU ARE FAIL: " + avg + "%");
+        //     System.out.println("TRY AGAIN NEXT YEAR");
+        // }
+        // System.out.println("HAVE A GOOD DAY");
+        // QUESTION 3;
+        // float tax = 0;
+        // System.out.println("ENTER YOUR INCOME IN LAKSH AS PER ANUAL INCOME");
+        // float  income;
+        // income = sc.nextFloat();
+        // if (income < 2.5f){
+        //     tax = tax + 0;
+
+        // }
+        // else if(income >= 2.5f && income <= 5f){
+        //     tax = tax +0.05f *(income-2.5f);
+        
+
+        // }
+        // else if(income > 5f && income<=10f){
+        //     tax = tax + 0.05f*(income-2.5f);
+        //     tax = tax + 0.2f*(income-5f);
+
+        // }
+        // else if(income>=1000000){
+        //     tax = tax + 0.05f*(income-2.5f);
+        //     tax = tax + 0.2f*(income-5f);
+        //     tax = tax + 0.3f*(income-10f);
+        // }
+//System.out.println("you tax amout is : "+tax);
+// QUESTION 4;
+// System.out.println("ENTER THE DAY NUMBER");
+// int day = sc.nextInt();
+// switch (day){
+//     case 1 -> System.out.println("MONDAY");
+//     case 2-> System.out.println("THURSDAY");
+//     case 3-> System.out.println("WEDNESDAY");
+//     case 4-> System.out.println("THURSDAY");
+//     case 5-> System.out.println("FRIDAY");
+//     case 6-> System.out.println("SATURDAY");
+//     case 7-> System.out.println("SUNDAY");
+//     default -> System.out.println("INVALID INPUT");
+//}    
+// QUESTION 5;
+// System.out.print( " ENTER YOUR YEAR; ");
+// int year = sc.nextInt();
+// //float a = year/4;
+
+// float a = year / 4;
+// if (year % 4 == 0) {
+//     System.out.println(year + " is a leap year.");
+// } else {
+//     System.out.println(year + " is not a leap year.");
+// }
+}
+
+}
