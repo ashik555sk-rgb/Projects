f8eaf4a1fae534c9719ca75437cb4abf3e8d5c1d
Aashiq <ashik555sk@gmail.com>
2025-10-13 21:09:54 +0530

    add the java file practice4

---
+++ Java/practice4.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class practice4 {
+    public static void main(String[] args) {
+        //question one print pattern
+        // int n = 4;
+        // for(int i=n; i>0; i--){
+        //     for (int j=0; j<i; j++){
+        //         System.out.print(" $");
+        //     }
+        //     System.out.print("\n");
+        // }
+// add first n even numbers;
+ Scanner input = new Scanner(System.in);
+// //System.out.println("ENTER YOUR NUMBER");
+// int n =  2;//input.nextInt();
+// int sum = 0;
+// for( int i = 0; i<n; i++){
+//     sum = sum + (2*i);
+//     // System.out.println( "SUM OF FIRST N NUMBER IS: ");
+//     System.out.println(sum);
+// }
+//  System.out.print("ENTER YOUR NUMBER: ");
+// int n = input.nextInt();
+// for (int i = 1; i<=10; i++){
+//     System.out.printf("%d x %d = %d\n", n ,i,n*i);
+    
+// }
+System.out.print("ENTER YOUR NUMBER: ");
+int n = input.nextInt();
+for (int i = 10; i>=1; i--){
+    System.out.printf("%d x %d = %d\n", n ,i,n*i);
+    
+}
+
+    }
+}
