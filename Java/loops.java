9c2e0f4f3e4c49fb49be03143d3cf39dfcdfae31
Aashiq <ashik555sk@gmail.com>
2025-10-13 18:40:31 +0530

    add the java file loops

---
+++ Java/loops.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] args) {
+       //while loop;
+            //  int i = 100;
+            //  while(i<=200){
+            //     System.out.println(i);
+            //     i++;
+            //  } 
+
+        //do while loop;
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<11);
+                                //for loop;
+         
+    
+    }
+    
+}
