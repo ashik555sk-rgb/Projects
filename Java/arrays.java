fa27fe3ac92c6466f080269bea727dde59256084
Aashiq <ashik555sk@gmail.com>
2025-10-15 11:36:45 +0530

    add the file multi_dim_array

--- Java/arrays.java
+++ Java/arrays.java
@@ -1,5 +1,5 @@
 public class arrays {
-    public static void main(String[] args) {
+    public static void main(String[] args, int element) {
         //method 1
         // int []marks = {1,2,3,4,5,};
         // System.out.println(marks[3]);
@@ -12,17 +12,35 @@ public class arrays {
 // marks[4]=5;
 // System.out.println(marks[3]);    
 //method 3;
-int []marks;
-marks= new int [6];
+// int []marks;
+// marks= new int [6];
 
-marks[0]=1;
-marks[1]=2;
-marks[2]=3;
-marks[3]=4; 
-marks[4]=5;
-marks[5]=6;
-System.out.println(marks[3]);
-
-    }
+// marks[0]=1;
+// marks[1]=2;
+// marks[2]=3;
+// marks[3]=4; 
+// marks[4]=5;
+// marks[5]=6;
+// System.out.println(marks[3]);
+int []marks = {1,2,3,4,5,};
+for(int mark: marks){
+    System.out.println(mark);
+}
+// for( int i =0; i<marks.length; i++){
+//     System.out.println(marks[i]);
+// }
+// for(int i = marks.length-1; i>=0; i--){
+//     System.out.println(marks[i]);
+// }
+//System.out.println(marks[3]);
+//string array
+// String [] student = {"rahul","nikhil",
+// "anand"};
+// System.out.println(student[1]);
+// boolean []arr = {true,false};
+// System.out.print(arr[1]);
+//     }
     
+}
+
 }
