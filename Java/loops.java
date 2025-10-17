6af82ac9eac7163ca9808067dfe56e25ae59ef85
Aashiq <ashik555sk@gmail.com>
2025-10-17 20:57:35 +0530

    add the java file pattrens

--- Java/loops.java
+++ Java/loops.java
@@ -1,32 +1,29 @@
 //import java.util.*;
 public class loops {
     public static void main(String[] args) {
-       //while loop;
-            //  int i = 100;
-            //  while(i<=200){
-            //     System.out.println(i);
-            //     i++;
-            //  } 
+        // while loop;
+        // int i = 100;
+        // while(i<=200){
+        // System.out.println(i);
+        // i++;
+        // }
 
-        //do while loop;
+        // do while loop;
         // int i = 0;
         // do{
-        //     System.out.println(i);
-        //     i++;
+        // System.out.println(i);
+        // i++;
         // }while(i<11);
-                              //  for loop;
-// int a= 10;
-//          for( int i = 0; i<=a; i++){
-         
-    
-//             System.out.println(2*i);
-//          }
-int n = 10;
-for(int i= 0; i>n; i--){
-    System.out.println(i);
-}
-}
-    
-    }
-    
+        // for loop;
+        // int a= 10;
+        // for( int i = 0; i<=a; i++){
 
+        // System.out.println(2*i);
+        // }
+        int n = 10;
+        for (int i = 0; i > n; i--) {
+            System.out.println(i);
+        }
+    }
+
+}
