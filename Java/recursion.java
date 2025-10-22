793e38ca6370b97a598007b1608c5f9e347ca7e1
Aashiq <ashik555sk@gmail.com>
2025-10-22 18:06:57 +0530

    add

---
+++ Java/recursion.java
@@ -0,0 +1,29 @@
+public class recursion {
+    static int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+//Ititrative method
+    static int fac(int a){
+        if (a==0 || a==1){
+            return 1;
+        }
+        else{
+            int product = 1;
+            for(int i =1;i<=a;i++){
+                product *= i;
+            }
+            return product;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(fac(6));
+    }
+    
+}
