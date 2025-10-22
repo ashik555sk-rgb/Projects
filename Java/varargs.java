3a718b6bf5b38cc0d7c64294b95240e23c09ae41
Aashiq <ashik555sk@gmail.com>
2025-10-22 17:48:49 +0530

    add

--- Java/varargs.java
+++ Java/varargs.java
@@ -1,5 +1,13 @@
 public class varargs{
+    static int sum ( int...arr){
+        int result = 0;
+        for(int a: arr){
+            result  +=a;
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        
+        System.out.println(sum ( 2,5,6,7,8,9));
+        System.out.println(sum(2,44,4,5,55,65,6));
     }
 }
